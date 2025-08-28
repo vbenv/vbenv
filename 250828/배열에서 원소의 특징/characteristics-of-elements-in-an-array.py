7fd507fa38a2596acfd7213c7e1975b7fc4f7603
vbenv <153050500+vbenv@users.noreply.github.com>
2025-08-28 17:59:29 +0900

    Title: 배열에서 원소의 특징, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250828/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
+++ 250828/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
@@ -1,6 +1,6 @@
 import sys
-n = list(map(int, sys.stdin.readline().strip().split()))\
+n = list(map(int, sys.stdin.readline().strip().split()))
 for i in range(1,10):
     if n[i] % 3 == 0:
-        sys.stdout.write(n[i-1])
+        sys.stdout.write(str(n[i-1]))
         break
