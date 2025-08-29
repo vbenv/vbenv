b2e87a0214b1f4a39d7cc8401a37c6fac881b8a0
vbenv <153050500+vbenv@users.noreply.github.com>
2025-08-29 13:30:49 +0900

    Title: 점수대 파악하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250829/점수대 파악하기/find-out-the-score-range.py
+++ 250829/점수대 파악하기/find-out-the-score-range.py
@@ -1,15 +1,8 @@
 n = list(map(int, input().split()))
-counts = [0] * 11 # 10점 단위 점수대 (10~19, 20~29, ..., 90~99)와 100점을 위한 배열
-
-for score in n:
-    if score == 0:
+tmp = [0]* 11
+for i in n:
+    if i == 0:
         break
-    if 10 <= score <= 99:
-        index = score // 10
-        counts[index] += 1
-    elif score == 100:
-        counts[10] += 1
-
-# 출력
-for i in range(10, 0, -1):
-    print(f"{i*10} - {counts[i]}")
+    tmp[(i//10)] += 1
+for idx in range(10,0,-1):
+    print(f"{(idx)*10} - {tmp[idx]}")
