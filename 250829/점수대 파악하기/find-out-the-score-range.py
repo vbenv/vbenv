0426d2016f2ede0a087a28f2fcefe8a109f105dd
vbenv <153050500+vbenv@users.noreply.github.com>
2025-08-29 13:45:46 +0900

    Title: 점수대 파악하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250829/점수대 파악하기/find-out-the-score-range.py
+++ 250829/점수대 파악하기/find-out-the-score-range.py
@@ -1,8 +1,8 @@
 n = list(map(int, input().split()))
-tmp = [0]* 11
+tmp = [0]* 10
 for i in n:
     if i == 0:
         break
-    tmp[(i//10)] += 1
-for idx in range(10,0,-1):
-    print(f"{(idx)*10} - {tmp7[idx]}")
+    tmp[(i//10)-1] += 1
+for idx in range(9,-1,-1):
+    print(f"{(idx+1)*10} - {tmp[idx]}")
