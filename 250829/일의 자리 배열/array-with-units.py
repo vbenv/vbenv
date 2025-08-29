cefa343f5b284a3a60ebaffb8987338801ff0474
vbenv <153050500+vbenv@users.noreply.github.com>
2025-08-29 09:54:55 +0900

    Title: 일의 자리 배열, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250829/일의 자리 배열/array-with-units.py
+++ 250829/일의 자리 배열/array-with-units.py
@@ -1,10 +1,9 @@
 import sys
 nn,n = map(int, sys.stdin.readline().strip().split())
 
-print(nn, n, end = ' ')
+sys.stdout.write(str(nn)+ ' ')
+sys.stdout.write(str(n)+ ' ')
 for _ in range(8):
-    nn, n = n, n+nn
-    if n >= 10:
-        n -= 10
-    print(abs(n), end = ' ')
+    nn, n = n, ((n+nn) % 10)
+    sys.stdout.write(str(n)+ ' ')
     
