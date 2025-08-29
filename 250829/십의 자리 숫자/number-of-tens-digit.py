b92f80d96fd6a22fe40d8ca24e4b0ef4b42242ca
vbenv <153050500+vbenv@users.noreply.github.com>
2025-08-29 11:17:19 +0900

    Title: 십의 자리 숫자, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250829/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,10 @@
+n = list(map(int,input().split()))
+
+for i in range(1,10):
+    cnt = 0
+    for elm in n:
+        if elm == 0:
+            continue
+        if int(elm*0.1) == i:
+            cnt += 1
+    print(f"{i} - {cnt}")
