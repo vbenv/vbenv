16792e0baf2abd8d09ef4ec681932dbdaf4a254a
vbenv <153050500+vbenv@users.noreply.github.com>
2025-08-29 11:20:39 +0900

    Title: 십의 자리 숫자, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250829/십의 자리 숫자/number-of-tens-digit.py
+++ 250829/십의 자리 숫자/number-of-tens-digit.py
@@ -4,7 +4,7 @@ for i in range(1,10):
     cnt = 0
     for elm in n:
         if elm == 0:
-            continue
+            break
         if int(elm*0.1) == i:
             cnt += 1
     print(f"{i} - {cnt}")
